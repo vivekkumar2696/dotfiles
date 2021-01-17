10fcf144b35d0e73490aa60fb521a9de7b279840
Vivek Kumar <kumar.vivek2696@gmail.com>
2021-01-18 01:18:24 +0530

    [setup.sh] Add setup.sh file to automate some of the basic tools required.

---
+++ setup.sh
@@ -0,0 +1,12 @@
+sudo apt update
+sudo apt install git
+sudo add-apt-repository universe
+sudo apt install gnome-tweak-tool
+sudo apt install zsh
+sudo apt install tmux
+wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh
+sh install.sh
+git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+
+
