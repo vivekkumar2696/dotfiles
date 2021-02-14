510b59ace2b0519abaab3e8526552d549bdc2d9e
Vivek Kumar <kumar.vivek2696@gmail.com>
2021-02-15 01:43:49 +0530

    [Setup] Add ansible and vagrant

--- setup.sh
+++ setup.sh
@@ -8,5 +8,9 @@ wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh
 sh install.sh
 git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+sudo apt install ansible
+sudo apt install virtualbox
+curl -O https://releases.hashicorp.com/vagrant/2.2.9/vagrant_2.2.9_x86_64.deb
+sudo apt install ./vagrant_2.2.9_x86_64.deb
 
 
