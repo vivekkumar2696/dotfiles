f195ccf55e8befe2a45111a1c7e92d6402abe32f
Vivek <kumar.vivek2696@gmail.com>
2021-03-13 16:33:21 +0530

    [setup.sh] Install and copy the dot files

--- setup.sh
+++ setup.sh
@@ -2,12 +2,20 @@ sudo apt update
 sudo apt install git
 sudo add-apt-repository universe
 sudo apt install gnome-tweak-tool
+sudo apt-get install gnome-tweak-tool
 sudo apt install zsh
-sudo apt install tmux
+sudo apt install tmux vim
 wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh
 sh install.sh
 git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+
+cp zsh/.zshrc ~/
+cp bash/.bashrc ~/
+cp tmux/.tmux.conf ~/
+tmux config ~/.tmux.conf
+cp vim/.vimrc ~/
+
 sudo apt install ansible
 sudo apt install virtualbox
 curl -O https://releases.hashicorp.com/vagrant/2.2.9/vagrant_2.2.9_x86_64.deb
